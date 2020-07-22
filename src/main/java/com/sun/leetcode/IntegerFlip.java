a14494aa168a46da529059241f1277cbeaa89ebf
1012191814@qq.com <1012191814@qq.com>
2020-07-22 10:27:19 +0800

    Initial commit

---
+++ src/main/java/com/sun/leetcode/IntegerFlip.java
@@ -0,0 +1,19 @@
+package com.sun.leetcode;
+
+/**
+ * LeetCode 第七题 整数翻转
+ */
+public class IntegerFlip {
+
+    public static void main(String[] args) {
+        int x = 123;
+        int reverse = reverse(x);
+        System.out.println(reverse);
+    }
+
+    public static int reverse(int x) {
+
+        
+        return 1;
+    }
+}
---
+++ src/main/java/com/sun/leetcode/SumOfTwoNumbers.java
@@ -0,0 +1,28 @@
+package com.sun.leetcode;
+
+/**
+ * LeetCode 第一题 两数之和
+ */
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 2, 4};
+        int target = 6;
+
+        int[] ints = twoSum(nums, target);
+        System.out.println(ints[0] + "-----" + ints[1]);
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}

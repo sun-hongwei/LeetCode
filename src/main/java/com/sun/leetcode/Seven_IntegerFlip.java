7599621b3fe794ca4142c0518c1fec93dfbd416a
翘楚 <1012191814@qq.com>
2020-12-23 09:32:37 +0800

    init

--- src/main/java/com/sun/leetcode/Seven_IntegerFlip.java
+++ src/main/java/com/sun/leetcode/Seven_IntegerFlip.java
@@ -11,9 +11,23 @@ public class Seven_IntegerFlip {
         System.out.println(reverse);
     }
 
+    /**
+     * 123
+     * 3 2 1
+     * @param x
+     * @return
+     */
     public static int reverse(int x) {
-
-
-        return 1;
+        int ans = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
+            ans = ans * 10 + pop;
+            x /= 10;
+        }
+        return ans;
     }
 }

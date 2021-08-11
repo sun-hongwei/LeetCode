bbb3300a4c7174bc5298b0a6386aa8bf82aba437
翘楚 <1012191814@qq.com>
2021-08-11 13:19:58 +0800

    init

--- src/main/java/com/sun/jni/HelloWorld.java
+++ src/main/java/com/sun/jni/HelloWorld.java
@@ -7,9 +7,10 @@ public class HelloWorld {
      * 1、javac ./com/sun/jni/HelloWorld.java
      * 2、生成C语言代码 javah -jni com.sun.jni.HelloWorld
      * 3、创建 jni_helloworldImpl.cpp
-     * 4、生成 .so 文件 Linux:
+     * 4、Linux:
      *    g++ -shared -I /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/
      *    JavaVM.framework/Versions/A/Headers jni_helloworldImpl.cpp -o Helloworld.so
+     *
      *    MACOS:
      *    gcc -shared -dynamiclib -I /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/
      *    JavaVM.framework/Versions/A/Headers -o libHelloWorld.dylib jni_helloworldImpl.cpp

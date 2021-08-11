2490aafa08f485d207ac112124887ad3a74a9ac8
翘楚 <1012191814@qq.com>
2021-08-11 13:18:04 +0800

    init

--- src/main/java/com/sun/jni/HelloWorld.java
+++ src/main/java/com/sun/jni/HelloWorld.java
@@ -7,13 +7,19 @@ public class HelloWorld {
      * 1、javac ./com/sun/jni/HelloWorld.java
      * 2、生成C语言代码 javah -jni com.sun.jni.HelloWorld
      * 3、创建 jni_helloworldImpl.cpp
-     * 4、生成 .so 文件 g++ -shared -I /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/
+     * 4、生成 .so 文件 Linux:
+     *    g++ -shared -I /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/
      *    JavaVM.framework/Versions/A/Headers jni_helloworldImpl.cpp -o Helloworld.so
+     *    MACOS:
+     *    gcc -shared -dynamiclib -I /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/
+     *    JavaVM.framework/Versions/A/Headers -o libHelloWorld.dylib jni_helloworldImpl.cpp
      */
 
     {
-        //绝对路径
-        System.load("/Users/hongweisun/Development/JavaDev/LeetCode/src/main/java/com/sun/jni/Helloworld.so");
+        //绝对路径 Linux
+        //System.load("/Users/hongweisun/Development/JavaDev/LeetCode/src/main/java/com/sun/jni/Helloworld.so");
+        //绝对路径 MacOS
+        System.load("/Users/hongweisun/Development/JavaDev/LeetCode/src/main/java/com/sun/jni/libHelloWorld.dylib");
     }
 
     public native void sayHello();

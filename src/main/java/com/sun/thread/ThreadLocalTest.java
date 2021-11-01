ea5fff2b086864e2647707a7d9392d1837b23d51
翘楚 <1012191814@qq.com>
2021-11-01 14:09:13 +0800

    init

---
+++ src/main/java/com/sun/thread/ThreadLocalTest.java
@@ -0,0 +1,36 @@
+package com.sun.thread;
+
+public class ThreadLocalTest {
+
+    static ThreadLocal<String> localVar = new ThreadLocal<>();
+
+    static void print(String str) {
+        //打印当前线程中本地内存中本地变量的值
+        System.out.println(str + " :" + localVar.get());
+        //清除本地内存中的本地变量
+        localVar.remove();
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            //设置线程1中本地变量的值
+            localVar.set("localVar1");
+            //调用打印方法
+            print("thread1");
+            //打印本地变量
+            System.out.println("after remove : " + localVar.get());
+        });
+
+        Thread t2 = new Thread(() -> {
+            //设置线程1中本地变量的值
+            localVar.set("localVar2");
+            //调用打印方法
+            print("thread2");
+            //打印本地变量
+            System.out.println("after remove : " + localVar.get());
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
